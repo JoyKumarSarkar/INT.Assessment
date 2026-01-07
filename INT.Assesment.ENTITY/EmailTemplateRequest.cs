9ac9e07b5110f1e1a214cf0749d54da02ae9c376
Joy Sarkar <connect2joysarkar@gmail.com>
2026-01-08 02:08:35 +0530

    INT Assessment Project

---
+++ INT.Assesment.ENTITY/EmailTemplateRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INT.Assessment.ENTITY
+{
+    internal class EmailTemplateRequest
+    {
+            public string Purpose { get; set; }
+            public string RecipientName { get; set; }
+            public string Tone { get; set; } // formal, friendly, professional
+    }
+}
